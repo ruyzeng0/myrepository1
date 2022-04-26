9718679f93d06b3bb7c653f67862bbcb0852aeca
ruyzeng0 <ruy.zeng@asiapac.com.cn>
2022-04-26 11:03:13 +0800

    fristcommit

---
+++ get_names.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/26 10:47
+# @Author  : ruy.zeng
+# @FileName: test1.py
+# @Software: PyCharm
+
+import names
+
+print(names.get_first_name()+" "+names.get_last_name())
